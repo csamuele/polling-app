ea7a978a88f48a40c910bbfa0cb218d3598c8b64
Samuel Coleman <samuel.e.c519@gmail.com>
2024-11-03 01:09:21 -0600

    add Question and Choice Models

--- polling_app/settings.py
+++ polling_app/settings.py
@@ -37,7 +37,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'polling_app',
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -116,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Denver'
 
 USE_I18N = True
 
