06c62a2ddf11ef742ea499e91f4719f8da32f36f
Samuel Coleman <samuel.e.c519@gmail.com>
2025-09-18 00:01:43 -0600

    linting

--- frontend/src/hooks/queries.ts
+++ frontend/src/hooks/queries.ts
@@ -1,16 +1,5 @@
-import { useQuery, QueryClient, useQueryClient } from '@tanstack/react-query';
-import type { ParamsOption, RequestBodyOption } from 'openapi-fetch';
-import type { paths } from '@lib/api/api';
 import $api from '@lib/api';
-import { useMutation } from '@tanstack/react-query';
 
-type UseQueryOptions<T> = ParamsOption<T> & 
-    RequestBodyOption<T> & {
-        reactQuery?: {
-            enabled?: boolean;
-            refetchInterval?: number;
-        }
-    }
 
 export function useQuestions() {
     const useGetQuestions = $api.useQuery(
--- frontend/src/hooks/useFetch.tsx
+++ frontend/src/hooks/useFetch.tsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
 
 const useFetch = <Message, Response>(url: string, method: 'get' | 'post' | 'put' | 'delete', successMessage: Message | null = null) => {
-    const [data, setData] = useState<Response | null>(null);
     const [isPending, setIsPending] = useState<boolean>(false);
     const [error, setError] = useState<string | null>(null);
-    const [message, setMessage] = useState<Message | null>(null);
 
-    const fetchData = async (data?: Message) => {
+    const fetchData = async (data?: Message, setData?: (response: Response | null) => void, setMessage?: (message: Message | null) => void) => {
         const headers = new Headers();
         headers.append('Content-Type', 'application/json');
         setIsPending(true);
@@ -20,8 +18,12 @@ const useFetch = <Message, Response>(url: string, method: 'get' | 'post' | 'put'
                 throw new Error('Failed to fetch data');
             }
             const responseData = await response.json();
-            setData(responseData);
-            setMessage(successMessage);
+            if (setData) {
+                setData(responseData);
+            }
+            if (setMessage && successMessage) {
+                setMessage(successMessage);
+            }
             setError(null);
         } catch (error) {
             if (error instanceof Error) {
@@ -29,13 +31,19 @@ const useFetch = <Message, Response>(url: string, method: 'get' | 'post' | 'put'
             } else {
                 setError('An unknown error occurred');
             }
-            setData(null);
-            setMessage(null);
+            if (setData) {
+                setData(null);
+            }
+            if (setMessage) {
+                setMessage(null);
+            }
         } finally {
             setIsPending(false);
         }
         return { data, isPending, error, fetchData };
-    }
+    };
+
+    return fetchData;
 }
 
 export default useFetch
